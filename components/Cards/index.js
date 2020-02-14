6221b57474728c95e8f8047cd1d7771bfdb754f9
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-14 11:47:49 -0500

    finished the MVp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,7 +6,7 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
+// {/* <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
 //     <div class="img-container">
@@ -14,6 +14,52 @@
 //     </div>
 //     <span>By {authors name}</span>
 //   </div>
-// </div>
+// </div> */}
 //
 // Create a card for each of the articles and add the card to the DOM.
+const container = document.querySelector('.cards-container');
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then( response => {
+const javascript = response.data.articles.javascript; 
+const bootstrap = response.data.articles.bootstrap; 
+const technology = response.data.articles.technology; 
+const jquery = response.data.articles.jquery; 
+const node = response.data.articles.node; 
+
+javascript.forEach(item => container.appendChild(cardCreatior(item)))
+bootstrap.forEach(item => container.appendChild(cardCreatior(item)))
+technology.forEach(item => container.appendChild(cardCreatior(item)))
+jquery.forEach(item => container.appendChild(cardCreatior(item)))
+node.forEach(item => container.appendChild(cardCreatior(item)))
+
+
+}).catch(err => console.log(err))
+
+function cardCreatior(obj){
+    //creating elements and assign variables for those elements and assigning classes
+    let card = document.createElement('div')
+    card.classList.add('card');
+    let headline = document.createElement('div');
+    headline.classList.add('headline');
+    let author = document.createElement('div');
+    author.classList.add('author');
+    let imageBox = document.createElement('div');
+    imageBox.classList.add('img-container')
+    let image = document.createElement('img');
+    let spanName = document.createElement('span');
+
+
+    // nesting elements 
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imageBox);
+    author.appendChild(spanName);
+    imageBox.appendChild(image);
+
+    //assigning content for object 
+    headline.textContent = obj.headline;
+    spanName.textContent = obj.authorName;
+    image.src = obj.authorPhoto;
+
+    return card
+
+}
